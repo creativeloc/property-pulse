02a0b193498000e37bb22b93a77fefccb59e17a6
creativeloc <creativeloc@yahoo.com>
2024-03-25 05:46:32 -0400

    add footer component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero"
 import InfoBoxes from "../components/InfoBoxes"
+import Footer from "@/components/Footer"
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <Footer />
     </>
   )
 }
