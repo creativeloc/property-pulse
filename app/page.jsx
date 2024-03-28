c24c06d61dcc9ea249febaea3df3f58f1e594bcc
creativeloc <creativeloc@yahoo.com>
2024-03-28 02:49:11 -0400

    install mongoDB and mongoose, create database connection

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,7 @@ import Hero from "@/components/Hero"
 import InfoBoxes from "@/components/InfoBoxes"
 import HomeProperties from "@/components/HomeProperties"
 
-const HomePage = () => {
-  console.log(process.env.MONGODB_URI)
+const HomePage = async () => {
   return (
     <>
       <Hero />
