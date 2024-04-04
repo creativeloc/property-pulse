5e1f435df9d83720e4c3ac45acbf307a701a3436
creativeloc <creativeloc@yahoo.com>
2024-04-04 07:21:16 -0400

    resume google auth

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import connectDB from "@/config/database"
+import User from "@/models/User"
+
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
@@ -18,15 +21,31 @@ export const authOptions = {
     // Invoked on Successful Sign-in
     async signIn({ profile }) {
       // 1. Connect to the database
+      await connectDB()
       // 2. Check is user exists
+      const userExists = await User.findOne({ email: profile.email })
       // 3. If not, then add user to database
+      if (!userExists) {
+        // Truncate username if too long
+        const username = profile.name.slice(0, 20)
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture
+        })
+      }
       // 4. Return true to allow sign in
+      return true
     },
     // Modifies the session object
     async session({ session }) {
       // 1. Get the user from the database
+      const user = await User.findOne({ email: session.user.email })
       // 2. Assign user id to the session
+      session.user.id = user._id.toString()
       // 3. Return the session
+      return session
     }
   }
 }
