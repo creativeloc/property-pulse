6773cfdc9d33be2bdf156e54acaf72a661a922d7
Lawrence Caulker <locaulker@gmail.com>
2024-06-01 01:31:09 -0400

    complete saved properties and bookmark functionality

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -11,10 +11,42 @@ const SavedPropertiesPage = () => {
   useEffect(() => {
     const fetchSavedProperties = async () => {
       try {
-      } catch (error) {}
+        const res = await fetch("/api/bookmarks")
+
+        if (res.status === 200) {
+          const data = await res.json()
+          setProperties(data)
+        } else {
+          console.log(res.statusText)
+          toast.error("Failed to fetch Saved Properties")
+        }
+      } catch (error) {
+        toast.error("Failed to fetch Saved Properties")
+      } finally {
+        setLoading(false)
+      }
     }
+
+    fetchSavedProperties()
   }, [])
 
-  return <div>SavedPropertiesPage</div>
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="px-4 py-6">
+      <h1 className="text-2xl mb-4">Saved Properties</h1>
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No Saved Properties</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
 export default SavedPropertiesPage
