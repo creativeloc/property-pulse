9838758b9881f23d3efc2a0dd17b4c251117460c
creativeloc <creativeloc@yahoo.com>
2024-06-03 01:44:23 -0400

    complete property search component

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,15 +1,24 @@
 "use client"
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 const PropertySearchForm = () => {
   const [location, setLocation] = useState("")
   const [propertyType, setPropertyType] = useState("All")
 
+  const router = useRouter()
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(propertyType, location)
-  }
 
+    if (location === "" && propertyType === "All") {
+      router.push("/properties")
+    } else {
+      const query = `?location=${location}&propertyType=${propertyType}`
+
+      router.push(`/properties/search-results${query}`)
+    }
+  }
   return (
     <form
       onSubmit={handleSubmit}
