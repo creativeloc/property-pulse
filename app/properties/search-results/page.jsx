ab3eac318dde074d79e4924d17f95848b2df9a5e
creativeloc <creativeloc@yahoo.com>
2024-06-07 00:39:15 -0400

    add search functionality to home page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,6 +1,10 @@
 "use client"
 import { useState, useEffect } from "react"
 import { useSearchParams } from "next/navigation"
+import Link from "next/link"
+import { FaArrowAltCircleLeft } from "react-icons/fa"
+import PropertyCard from "@/components/PropertyCard"
+import Spinner from "@/components/Spinner"
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams()
@@ -34,8 +38,29 @@ const SearchResultsPage = () => {
     fetchSearchResults()
   }, [location, propertyType])
 
-  console.log(properties)
-
-  return <div>SearchResultsPage</div>
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <Link
+          href="/properties"
+          className="flex items-center text-blue-500 hover:underline mb-3"
+        >
+          <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Properties
+        </Link>
+        <h1 className="text-2xl mb-4">Search Results</h1>
+        {properties.length === 0 ? (
+          <p>No Search results found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
 export default SearchResultsPage
