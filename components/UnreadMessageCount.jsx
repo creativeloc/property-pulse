a62eeddc9d91dfd00a10c8e9b44e5a831b9db051
creativeloc <creativeloc@yahoo.com>
2024-06-23 01:51:00 -0400

    add global context; complete messages functionalities

--- components/UnreadMessageCount.jsx
+++ components/UnreadMessageCount.jsx
@@ -1,8 +1,9 @@
 "use client"
 import { useState, useEffect } from "react"
+import { useGlobalContext } from "@/context/GlobalContext"
 
 const UnreadMessageCount = ({ session }) => {
-  const [unreadCount, setUnreadCount] = useState(0)
+  const { unreadCount, setUnreadCount } = useGlobalContext(0)
 
   useEffect(() => {
     if (!session) return
